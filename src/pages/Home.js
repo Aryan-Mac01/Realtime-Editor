c8060c2f4f51f45fe941c6b2921cc18768e6a7c1
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-04-25 10:26:53 +0530

    home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,36 @@ import React from 'react'
 
 const Home = () => {
     return (
-    <div>
-        Home Page
+    <div className='homePageWrapper'>
+        <div className='formWrapper'>
+            <img src="/code-sync.png" alt="code-sync-logo" />
+            <h4 className="mainLabel">Paste invitation ROOM ID</h4>
+            <div className="inputGroup">
+                <input 
+                    type="text" 
+                    className="inputBox" 
+                    placeholder="ROOM ID"
+                />
+                <input 
+                    type="text" 
+                    className="inputBox" 
+                    placeholder="USERNAME"
+                />
+            <button className="btn joinBtn">Join</button>
+            <span className="createInfo">
+                If you don't have an invite then create &nbsp;
+                <a href="" className="createNewBtn">
+                    new room
+                </a>
+            </span>
+        </div>
+        </div>
+        
+        <footer>
+            <h4>
+                Built by <a href="https://github.com/Aryan-Mac01">Aryan</a>
+            </h4>
+        </footer>
     </div>  
     ) 
 }
